fa34724751a2d77c49b540bc878f5a14452af5e1
[garimaD-dev] <dwivedigarima2202@gmail.com>
2026-08-15 21:42:01 +0530

    config: setup MongoDB and Redis connections

---
+++ product/src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+
+async function connectToDB(){
+    try{
+        mongoose.connect(process.env.MONGODB_URL);
+        console.log("Database connection successful");
+    }
+    catch(err){
+        console.log("Database connection failed",err);
+    }
+}
+
+module.exports=connectToDB;
