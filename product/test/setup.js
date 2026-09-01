8a0aed893dd67a16be4855b8c9fdd31663ba0f11
[garimaD-dev] <dwivedigarima2202@gmail.com>
2026-09-01 19:01:09 +0530

    feat: add test setup for MongoDB in-memory server

---
+++ product/test/setup.js
@@ -0,0 +1,14 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  await mongoose.connect(mongoServer.getUri());
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
