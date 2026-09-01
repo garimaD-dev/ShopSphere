4050210394962b34307af5e3bba21a5b0daa0dc7
[garimaD-dev] <dwivedigarima2202@gmail.com>
2026-09-01 19:02:38 +0530

    feat: add ImageKit service for image uploads

---
+++ product/src/service/imagekit.service.js
@@ -0,0 +1,26 @@
+const ImageKit = require('imagekit');
+const {v4 : uuidv4}= require('uuid');
+
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY || '',
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY || '',
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || '',
+});
+
+async function uploadImage({ buffer, filename, folder = '/products' }) {
+  const res = await imagekit.upload({
+    file: buffer,
+    fileName: filename || uuidv4(),
+    folder,
+  });
+  return {
+    url: res.url,
+    thumbnail: res.thumbnailUrl || res.url,
+    id: res.fileId,
+  };
+}
+
+module.exports = {
+  imagekit,
+  uploadImage
+};
